24f68e823172d3f0bd3af38c3fc01d325ad5fb41
Alejandro Gispert <alejandrobusiness2022@gmail.com>
2026-03-16 11:14:52 +0100

    Update BIRTH_DATE

--- scripts/evolve.sh
+++ scripts/evolve.sh
@@ -22,7 +22,8 @@ DATE=$(date +%Y-%m-%d)
 SESSION_TIME=$(date +%H:%M)
 
 # Day count (days since project start)
-BIRTH_DATE="${BIRTH_DATE:2026-03-15}"
+# Use BIRTH_DATE if set, otherwise default to project start date.
+BIRTH_DATE="${BIRTH_DATE:-2026-03-15}"
 if date -j &>/dev/null 2>&1; then
   DAY=$(( ($(date +%s) - $(date -j -f "%Y-%m-%d" "$BIRTH_DATE" +%s)) / 86400 ))
 else
